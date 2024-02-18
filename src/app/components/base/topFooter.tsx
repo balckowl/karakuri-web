e6158d902fc8877d3052d1d97dd0f20fd191a4d0
kusira <takaaki200302@gmail.com>
2024-02-18 18:06:42 +0900

    add: トップページ仮完成

--- src/app/components/base/topFooter.tsx
+++ src/app/components/base/topFooter.tsx
@@ -6,7 +6,7 @@ const TopFooter = () => {
     <div className="container border-black border-t-[1px] pt-10">
       <div className="flex mb-12">
         <div className="w-[30%]">
-          <Image src={"/images/karakuri-web_logo.png"} alt="gyudon" width={230} height={230} className="mx-auto"/>
+          <Image src={"/images/karakuri-web_logo.png"} alt="gyudon" width={230} height={230} className="mx-auto" />
           <p className="text-center">karakuri web</p>
         </div>
         <div className="flex w-[70%] justify-around pt-10">
@@ -15,7 +15,7 @@ const TopFooter = () => {
 
             <div>
               <div className="flex justify-between mb-4">
-                <Image src={"/images/gyudon.png"} alt="牛丼" width={70} height={70} className="rounded-[50%]"/>
+                <Image src={"/images/gyudon.png"} alt="牛丼" width={70} height={70} className="rounded-[50%]" />
                 <div className="w-[50%]">
                   <p className="font-semibold">牛丼</p>
                   <p className="text-sm mb-2 text-[#444]">gyudon</p>
@@ -27,17 +27,17 @@ const TopFooter = () => {
             </div>
 
             <div className="flex gap-4">
-              <a href="#" className="hover:opacity-80 transition-all"><Image src={"/images/twitter.png"} alt="twitter" width={30} height={30} className="rounded-[10px]"/></a>
-              <a href="#" className="hover:opacity-80 transition-all"><Image src={"/images/bluesky.png"} alt="bluesky" width={30} height={30} className="rounded-[10px]"/></a>
+              <a href="#" className="hover:opacity-80 transition-all"><Image src={"/images/twitter.png"} alt="twitter" width={30} height={30} className="rounded-[10px]" /></a>
+              <a href="#" className="hover:opacity-80 transition-all"><Image src={"/images/bluesky.png"} alt="bluesky" width={30} height={30} className="rounded-[10px]" /></a>
             </div>
           </div>
 
           {/* くしらっちょ */}
           <div className="w-[30%] flex flex-col h-full justify-between">
-            
+
             <div>
               <div className="flex justify-between mb-4">
-                <Image src={"/images/kusirattyo.jpg"} alt="kusirattyo" width={70} height={70} className="rounded-[50%]"/>
+                <Image src={"/images/kusirattyo.jpg"} alt="kusirattyo" width={70} height={70} className="rounded-[50%]" />
                 <div className="w-[50%]">
                   <p className="font-semibold">くしらっちょ</p>
                   <p className="text-sm mb-2 text-[#444]">kusirattyo</p>
@@ -49,7 +49,7 @@ const TopFooter = () => {
             </div>
 
             <div className="flex gap-4">
-              <a href="#" className="hover:opacity-80 transition-all"><Image src={"/images/twitter.png"} alt="twitter" width={30} height={30} className="rounded-[10px]"/></a>
+              <a href="#" className="hover:opacity-80 transition-all"><Image src={"/images/twitter.png"} alt="twitter" width={30} height={30} className="rounded-[10px]" /></a>
             </div>
           </div>
         </div>
