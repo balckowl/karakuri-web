5ca51f8ab9dd36b6e7208e73905387cecbcb1c13
kusira <takaaki200302@gmail.com>
2024-02-18 18:02:12 +0900

    add: トップページ仮完成しました

--- src/app/components/base/about.tsx
+++ src/app/components/base/about.tsx
@@ -1,12 +1,44 @@
-import React from "react"
+
+import Image from "next/image";
 
 const About = () => {
   return (
-    <div className= "container">
-      <div>
-        <p className="text-[#AE8818]">内容</p>
-        <h2 className="text-[50px]">ABOUT</h2>
-        <p className="ml-1">karakuri-webの特徴</p>
+    <div className="container">
+      {/* 見出し */}
+      <div className="mb-20 lg:mb-28">
+        <p className="text-[#AE8818] lg:text-[18px]">・内容</p>
+        <h2 className="text-[50px] leading-[50px] mb-2 font-bold">ABOUT</h2>
+        <p className="ml-1 lg:text-[18px]">karakuri-webの特徴。karakuri-webの特徴。karakuri-webの特徴。</p>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="w-full border-black border-b-[1px] flex justify-between mb-16">
+          <div className="w-[40%] max-w-[400px]">
+            <Image src={"/images/sub1.webp"} alt="down" width={300} height={300} className="select-none w-full h-full object-cover" />
+          </div>
+          <div className="w-[50%]">
+            <h3 className="text-[24px] mb-2 lg:text-[32px]">① 豊富なギミック</h3>
+            <p className="mx-2 lg:text-[18px]">チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ ブーブーブーチピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ　</p>
+          </div>
+        </div>
+        <div className="w-full border-black border-b-[1px] flex flex-row-reverse justify-between mb-16">
+          <div className="w-[40%] max-w-[400px]">
+            <Image src={"/images/sub2.webp"} alt="down" width={300} height={300} className="select-none w-full h-full object-cover" />
+          </div>
+          <div className="w-[50%]">
+            <h3 className="text-[24px] mb-2 lg:text-[32px]">② AIによるサポート</h3>
+            <p className="mx-2 lg:text-[18px]">チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ　チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ ブーブーブー</p>
+          </div>
+        </div>
+        <div className="w-full border-black border-b-[1px] flex justify-between mb-16">
+          <div className="w-[40%] max-w-[400px]">
+            <Image src={"/images/sub3.webp"} alt="down" width={300} height={300} className="select-none w-full h-full object-cover" />
+          </div>
+          <div className="w-[50%]">
+            <h3 className="text-[24px] mb-2 lg:text-[32px]">③ ページのカスタマイズ</h3>
+            <p className="mx-2 lg:text-[18px]">チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ　チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ ブーブーブー</p>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/app/components/base/hero.tsx
+++ src/app/components/base/hero.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+
 import Image from "next/image";
 import { Button } from "@/app/components/ui/button"
 import Link from "next/link"
@@ -6,7 +6,7 @@ import Link from "next/link"
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-[80vh] flex flex-col justify-end items-center mb-10">
+    <div className="relative w-full h-[100vh] lg:h-[70vh] flex flex-col justify-end items-center mb-10">
     <Image
       src={"/images/karakuri-web_main.webp"}
       alt="main"
@@ -16,16 +16,16 @@ const Hero = () => {
     />
     <div className="absolute w-full h-full bg-black bg-opacity-80  z-50">
       <div className="w-max mx-auto h-full flex flex-col justify-center translate-y-[-16%]">
-        <p className="text-3xl text-white mb-2">ようこそ、からくり館へ</p>
-        <p className="text-white mb-20">
+        <p className="text-3xl text-white font-bold mb-8 md:text-[40px] lg:text-[60px] lg:mb-12">ようこそ、からくり館へ</p>
+        <p className="text-white mb-20 lg:text-[20px]">
         あるサイトにログインしたとき、ある屋敷に閉じ込められてしまった。<br />
         どうやらこの屋敷にはWebを用いたからくりがあるらしい...<br />
         それらを攻略し、webサイトから脱出しよう。<br />
         想定プレイ時間: 1時間
         </p>
         <div className="w-full flex justify-end">
-          <Button className=" bg-transparent border-white border-[1px] text-white hover:opacity-80 hover:bg-transparent transition-all w-max">
-            <Link href="/auth/login">始めよう</Link>
+          <Button className=" bg-transparent border-white border-[1px] text-white hover:opacity-80 hover:bg-transparent transition-all w-max p-6 lg:p-8">
+            <Link href="/auth/login" className="text-xl">脱出する</Link>
           </Button>
         </div>
       </div>
