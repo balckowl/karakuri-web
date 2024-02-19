44eef5e270af251063179efc5bc1140aabc90059
K.A <>
2024-02-19 02:05:41 -0800

    update: newsがtopページに表示されるようにしました。

--- src/app/components/base/news.tsx
+++ src/app/components/base/news.tsx
@@ -1,8 +1,22 @@
 import { Button } from "@/app/components/ui/button"
 import Link from "next/link"
+import { parseISO, format } from 'date-fns'
+
+const getNewsData = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/news`, {
+    next: { revalidate: 60 * 60 * 24 }
+  })
+
+  const newsData = await res.json()
+
+  return newsData
+}
 
 
-const News = () => {
+const News = async() => {
+
+  const newsDataList = await getNewsData()
+
   return (
     <div className="container">
       {/* 見出し */}
@@ -12,23 +26,19 @@ const News = () => {
       </div>
 
       <div className="flex w-10/12 mx-auto flex-col mb-20 lg:mb-32">
-        <div className="border-black border-b-[1px] w-full flex justify-between pb-2 mb-10 lg:pb-4 lg:mb-16">
-          <p className="w-4/12 lg:text-[18px]">2024.1.1</p>
-          <p className="w-8/12 max-w-[300px] lg:text-[18px]">ちぴちぴをちゃぱちゃぱしました。</p>
-        </div>
-        <div className="border-black border-b-[1px] w-full flex justify-between pb-2 mb-10 lg:pb-4 lg:mb-16">
-          <p className="w-4/12 lg:text-[18px]">2024.1.1</p>
-          <p className="w-8/12 max-w-[300px] lg:text-[18px]">Happy Happy Happy</p>
-        </div>
-        <div className="border-black border-b-[1px] w-full flex justify-between pb-2 mb-10 lg:pb-4 lg:mb-16">
-          <p className="w-4/12 lg:text-[18px]">2024.1.1</p>
-          <p className="w-8/12 max-w-[300px] lg:text-[18px]">hum?</p>
-        </div>
-        <div className="flex justify-end">
+
+        {newsDataList.map((newsData: any, index: number) => (
+          <div className="border-black border-b-[1px] w-full flex justify-between pb-2 mb-10 lg:pb-4 lg:mb-16" key={index}>
+            <p className="w-3/12 lg:text-[18px]">{format(new Date(newsData.createdAt._seconds * 1000), 'yyyy.MM.dd')}</p>
+            <p className="w-9/12 max-w-[350px] lg:text-[18px]">{newsData.content}</p>
+          </div>
+        ))}
+
+        {/* <div className="flex justify-end">
           <Button className="bg-white border-black border-[1px] text-black hover:text-white w-max p-6 lg:p-8">
             <Link href="/auth/login">始めよう</Link>
           </Button>
-        </div>
+        </div> */}
       </div>
     </div>
   )
