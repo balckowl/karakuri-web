fb6638fbb2abfc9dc7998ccfecb63f36f5e6ab88
kusira <takaaki200302@gmail.com>
2024-02-20 18:20:19 +0900

    add: ログインページに利用規約の記入

--- src/app/(pages)/auth/login/page.tsx
+++ src/app/(pages)/auth/login/page.tsx
@@ -15,7 +15,6 @@ const Login = () => {
     const { data: session, status } = useSession()
     const router = useRouter()
 
-
     const googleProvider = new GoogleAuthProvider
 
     const signInWithGoogle = () => {
@@ -49,6 +48,45 @@ const Login = () => {
                         <Image src="/images/auth/google-icon.svg" width={20} height={20} alt="google-icon" />
                         <p>Sign In with Google</p>
                     </Button>
+                </div>
+            </div>
+            <div className="w-[400px] mx-auto">
+                <p className="mt-20">利用規約</p>
+                <div className=" h-[300px] overflow-hidden overflow-y-scroll border-black border-[1px] mx-auto p-4">
+<h4 className="text-2xl font-bold mb-4">お客様から取得する情報</h4>
+    <ul className="list-disc ml-8">
+        <li>氏名(ニックネームやペンネームも含む)</li>
+        <li>メールアドレス</li>
+        <li>Cookie(クッキー)を用いて生成された識別情報</li>
+    </ul>
+
+    <p className="text-2xl font-bold mt-4">お客様の情報を利用する目的</p>
+    <p className="mt-4">当ウェブサイトは、お客様から取得した情報を、以下の目的のために利用します。</p>
+    <ul className="list-disc ml-8">
+        <li>当ウェブサイトサービスの提供、維持、保護及び改善のため</li>
+    </ul>
+
+    <p className="text-2xl font-bold mt-4">第三者提供</p>
+    <p className="mt-4">当ウェブサイトは、お客様から取得する情報のうち、個人データ（個人情報保護法第１６条第３項）に該当するものついては、あらかじめお客様の同意を得ずに、第三者（日本国外にある者を含みます。）に提供しません。</p>
+    <ul className="list-disc ml-8">
+        <li>個人データの取扱いを外部に委託する場合</li>
+        <li>当ウェブサイトや当ウェブサイトサービスが買収された場合</li>
+        <li>事業パートナーと共同利用する場合（具体的な共同利用がある場合は、その内容を別途公表します。）</li>
+        <li>その他、法律によって合法的に第三者提供が許されている場合</li>
+    </ul>
+
+    <p className="text-2xl font-bold mt-4">アクセス解析ツール</p>
+    <p className="mt-4">当ウェブサイトは、お客様のアクセス解析のために、「Googleアナリティクス」を利用しています。</p>
+    <p>Googleアナリティクスは、トラフィックデータの収集のためにCookieを使用しています。トラフィックデータは匿名で収集されており、個人を特定するものではありません。</p>
+    <p>Cookieを無効にすれば、これらの情報の収集を拒否することができます。詳しくはお使いのブラウザの設定をご確認ください。</p>
+    <p>Googleアナリティクスについて、詳しくは以下からご確認ください。<a className="text-blue-500" href="https://marketingplatform.google.com/about/analytics/terms/jp/">https://marketingplatform.google.com/about/analytics/terms/jp/</a></p>
+
+    <p className="text-2xl font-bold mt-4">プライバシーポリシーの変更</p>
+    <p className="mt-4">当ウェブサイトは、必要に応じて、このプライバシーポリシーの内容を変更します。この場合、変更後のプライバシーポリシーの施行時期と内容を適切な方法により周知または通知します。</p>
+
+    <p className="mt-4">ハッカソン名: からくリンゴ</p>
+    <p>2024年02月20日 制定</p>
+
                 </div>
             </div>
         </div>
--- src/app/components/base/about.tsx
+++ src/app/components/base/about.tsx
@@ -12,31 +12,34 @@ const About = () => {
       </div>
 
       <div className="flex flex-col">
-        <div className="w-full border-black border-b-[1px] flex justify-between mb-16">
+        <div className="w-full border-[#ae8818] flex justify-between mb-16 gap-6">
           <div className="w-[40%] max-w-[400px]">
-            <Image src={"/images/sub1.webp"} alt="down" width={300} height={300} className="select-none w-full h-full object-cover" />
+            <Image src={"/images/sub1.webp"} alt="sub1" width={300} height={300} className="max-w-[300px] select-none w-full h-full object-cover" />
           </div>
-          <div className="w-[50%]">
-            <h3 className="text-[24px] mb-2 lg:text-[32px]">① 豊富なギミック</h3>
-            <p className="mx-2 lg:text-[18px]">チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ ブーブーブーチピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ　</p>
+          <div className="w-[60%]">
+            <h3 className="text-[24px] lg:text-[32px]">豊富なギミック</h3>
+            <div className="w-[30px] h-[4px] bg-[#ae8818] mb-2"></div>
+            <p className="mx-2 lg:text-[18px]">多彩なギミックを取り揃え、謎解きのバリエーションが豊富です。</p>
           </div>
         </div>
-        <div className="w-full border-black border-b-[1px] flex flex-row-reverse justify-between mb-16">
+        <div className="w-full border-[#ae8818] flex flex-row-reverse justify-between mb-16">
           <div className="w-[40%] max-w-[400px]">
-            <Image src={"/images/sub2.webp"} alt="down" width={300} height={300} className="select-none w-full h-full object-cover" />
+            <Image src={"/images/sub2.webp"} alt="sub2" width={300} height={300} className="max-w-[300px] select-none w-full h-full object-cover" />
           </div>
-          <div className="w-[50%]">
-            <h3 className="text-[24px] mb-2 lg:text-[32px]">② AIによるサポート</h3>
-            <p className="mx-2 lg:text-[18px]">チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ　チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ ブーブーブー</p>
+          <div className="w-[60%]">
+            <h3 className="text-[24px] mb-2 lg:text-[32px]">AIによるサポート</h3>
+            <div className="w-[30px] h-[4px] bg-[#ae8818] mb-2"></div>
+            <p className="mx-2 lg:text-[18px]">ゲーム内でAIが独自のヒントや解説を提供します。プレイヤーが行き詰まった際にスマートかつ効果的なサポートが得られ、ストレスなく謎解きを進めれます。</p>
           </div>
         </div>
-        <div className="w-full border-black border-b-[1px] flex justify-between mb-16">
+        <div className="w-full border-[#ae8818] flex justify-between mb-16">
           <div className="w-[40%] max-w-[400px]">
-            <Image src={"/images/sub3.webp"} alt="down" width={300} height={300} className="select-none w-full h-full object-cover" />
+            <Image src={"/images/sub3.webp"} alt="sub3" width={300} height={300} className="max-w-[300px] select-none w-full h-full object-cover" />
           </div>
-          <div className="w-[50%]">
-            <h3 className="text-[24px] mb-2 lg:text-[32px]">③ ページのカスタマイズ</h3>
-            <p className="mx-2 lg:text-[18px]">チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ　チピチピ チャパチャパ ルビルビ ラバラバ パチコミルビルビ ブーブーブー</p>
+          <div className="w-[60%]">
+            <h3 className="text-[24px] mb-2 lg:text-[32px]">ページのカスタマイズ</h3>
+            <div className="w-[30px] h-[4px] bg-[#ae8818] mb-2"></div>
+            <p className="mx-2 lg:text-[18px]">プレイヤーは自らの好みに合わせてゲームページをカスタマイズできます。個性的な雰囲気や難易度設定で、カスタム体験を楽しみましょう。</p>
           </div>
         </div>
       </div>
